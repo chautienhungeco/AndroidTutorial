d9ef11236ca9f967b19bbebcc3bd5dcc017c3981
hunggo <92900556+hunggo@users.noreply.github.com>
2025-10-16 09:12:52 +0700

    push code 16-10 chỉnh sửa setting_activity

--- app/src/main/java/com/example/androidtutorial/DemoLayoutActivity.kt
+++ app/src/main/java/com/example/androidtutorial/DemoLayoutActivity.kt
@@ -1,11 +1,28 @@
 package com.example.androidtutorial
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.androidtutorial.auth.LoginActivity
 
-class DemoLayoutActivity : AppCompatActivity(){
+class DemoLayoutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_demo_map)
+
+        val btnGoToLogin = findViewById<Button>(R.id.btnGoToLogin)
+        val imgSetting = findViewById<ImageView>(R.id.btnSetting)
+//
+        btnGoToLogin.setOnClickListener {
+            val loginIntent = Intent(this, LoginActivity::class.java)
+            startActivity(loginIntent)
+        }
+
+        imgSetting.setOnClickListener {
+            val settingIntent = Intent(this, DemoSettingActivity::class.java)
+            startActivity(settingIntent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/androidtutorial/DemoSettingActivity.kt
@@ -0,0 +1,11 @@
+package com.example.androidtutorial
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DemoSettingActivity: AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting)
+    }
+}
